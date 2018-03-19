d93f72440b8adade1fcadeca2b507053f8c40b32
maiya-22 <maiya_o@yahoo.com>
2018-03-19 15:14:03 -0400

    basic grid in place.  event-listeners get generic elment ids and classNames from selection buttons on form.

--- scripts/main-scripts.js
+++ scripts/main-scripts.js
@@ -1,4 +1,47 @@
 
 window.onload = function(evt){
     console.log(`js loaded`);
+    const body = document.getElementsByTagName('body')[0];
+
+    let button = document.getElementById('set-style');
+
+    //re-factor this as reusable function:
+    let containerElements = 'frame one two three four five six'.split(' ');
+    let elementSelector = document.getElementById('select-element');
+    containerElements.forEach((element, index) => {
+        let newSelect = document.createElement('option');
+        newSelect.innerHTML = element;
+        newSelect.setAttribute('value', element);
+        elementSelector.appendChild(newSelect);
+    });
+
+    let classNames = 'this that and the other'.split(' ');
+    let classSelector = document.getElementById('select-class');
+    classNames.forEach((className, index) => {
+        let newSelect = document.createElement('option');
+        newSelect.innerHTML = className;
+        newSelect.setAttribute('value', className);
+        classSelector.appendChild(newSelect);
+    });
+
+    button.addEventListener('click', function(evt) {
+        evt.preventDefault();
+        let selectedElement = document.getElementById( "select-element" );
+        let selectClassName = document.getElementById("select-class");
+        let elementId = selectedElement.value;
+        let element = document.getElementById(elementId);
+        console.log('element: ', element);
+        alert(element);
+
+
+
+    });
+   
+
+
+
+
+    
+    
+
 }
