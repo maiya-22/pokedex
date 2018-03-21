e8ae004843b4be79ca977cb077392d8e7c14c37e
maiya-22 <maiya_o@yahoo.com>
2018-03-21 11:48:04 -0400

    saving progress.  grids are now set to the map#1 drawing (in wireframe).  Now going to change grids.

--- scripts/main-scripts-2.js
+++ scripts/main-scripts-2.js
@@ -1,3 +1,20 @@
-document.onload = function() {
-    
-};
+
+$(function() {
+    console.log('connected');
+
+    let pokemonDisplay = document.getElementById('pokemonDisplay');
+    //select an item and assign a click function to it:
+    let goButton = document.getElementById('goButton');
+    goButton.addEventListener('click', function(evt) {
+        // $.ajax()
+        let name = document.getElementById('search').value;
+        name = name.toLowerCase();
+        // $.load();
+        console.log(name);
+    });
+});
+
+
+
+
+
