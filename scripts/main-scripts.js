a407b5d5f1aeba91ac505bb447bc11ca4fe32def
maiya-22 <maiya_o@yahoo.com>
2018-03-23 11:08:41 -0400

    combined files, deleted extra files and extra code

--- scripts/main-scripts.js
+++ scripts/main-scripts.js
@@ -1,47 +1,62 @@
+/* eslint func-names: 0  no-unused-vars: 0 */
+$(() => {
+  // #POKEMON API CODE:
 
-window.onload = function(evt){
-    console.log(`js loaded`);
-    const body = document.getElementsByTagName('body')[0];
+  console.log('connected');
+  const pokemonDisplay = document.getElementById('pokemonDisplay');
+  console.log(pokemonDisplay);
+  // select an item and assign a click function to it:
+  const goButton = document.getElementById('goButton');
+  goButton.addEventListener('click', (evt) => {
+    // $.ajax()
+    let name = document.getElementById('search').value;
+    name = name.toLowerCase();
+  });
 
-    let button = document.getElementById('set-style');
+  // make a list of stats to find and also change color of buttons when clicked:
+  //   const statButtons = document.querySelectorAll('#pokemonStats div');
+  //   const statQuery = {
+  //     name: false,
+  //     power: false,
+  //   };
+  const pokedexWrap = document.getElementById('pokedexWrap');
+  pokedexWrap.addEventListener('click', (e) => {
+    const target = e.target;
+    const data = target.getAttribute('data');
+    target.classList.add('pressed');
+    console.log(target);
+  });
 
-    //re-factor this as reusable function:
-    let containerElements = 'frame one two three four five six'.split(' ');
-    let elementSelector = document.getElementById('select-element');
-    containerElements.forEach((element, index) => {
-        let newSelect = document.createElement('option');
-        newSelect.innerHTML = element;
-        newSelect.setAttribute('value', element);
-        elementSelector.appendChild(newSelect);
-    });
+  const backgroundImages = '0001 0002 0003 0004'.split(' ').map(imageName => `${imageName}.jpg`);
 
-    let classNames = 'this that and the other'.split(' ');
-    let classSelector = document.getElementById('select-class');
-    classNames.forEach((className, index) => {
-        let newSelect = document.createElement('option');
-        newSelect.innerHTML = className;
-        newSelect.setAttribute('value', className);
-        classSelector.appendChild(newSelect);
-    });
+  // #SET STYLE CODE:
+  const button = document.getElementById('set-style');
+  // re-factor this as reusable function:
+  const containerElements = 'frame one two three four five six'.split(' ');
+  const elementSelector = document.getElementById('select-element');
+  containerElements.forEach((element, index) => {
+    const newSelect = document.createElement('option');
+    newSelect.innerHTML = element;
+    newSelect.setAttribute('value', element);
+    elementSelector.appendChild(newSelect);
+  });
 
-    button.addEventListener('click', function(evt) {
-        evt.preventDefault();
-        let selectedElement = document.getElementById( "select-element" );
-        let selectClassName = document.getElementById("select-class");
-        let elementId = selectedElement.value;
-        let element = document.getElementById(elementId);
-        console.log('element: ', element);
-        alert(element);
+  const classNames = 'this that and the other'.split(' ');
+  const classSelector = document.getElementById('select-class');
+  classNames.forEach((className, index) => {
+    const newSelect = document.createElement('option');
+    newSelect.innerHTML = className;
+    newSelect.setAttribute('value', className);
+    classSelector.appendChild(newSelect);
+  });
 
-
-
-    });
-   
-
-
-
-
-    
-    
-
-}
+  button.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    const selectedElement = document.getElementById('select-element');
+    const selectClassName = document.getElementById('select-class');
+    const elementId = selectedElement.value;
+    const element = document.getElementById(elementId);
+    console.log('element: ', element);
+    alert(element);
+  });
+});
