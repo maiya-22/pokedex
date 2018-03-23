933c1b33367e2534f3fa4135383f29160780f713
maiya-22 <maiya_o@yahoo.com>
2018-03-23 10:58:06 -0400

    saving progress before removing extra pages with extra code.  There were pages, where I was experimenting with CSS Grid.  Removing those.

--- scripts/main-scripts-2.js
+++ scripts/main-scripts-2.js
@@ -1,20 +1,34 @@
+/* eslint func-names: 0  no-unused-vars: 0 */
+$(() => {
+  console.log('connected');
+  const pokemonDisplay = document.getElementById('pokemonDisplay');
+  console.log(pokemonDisplay);
+  // select an item and assign a click function to it:
+  const goButton = document.getElementById('goButton');
+  goButton.addEventListener('click', (evt) => {
+    // $.ajax()
+    let name = document.getElementById('search').value;
+    name = name.toLowerCase();
+  });
 
-$(function() {
-    console.log('connected');
+  // make a list of stats to find and also change color of buttons when clicked:
+  //   const statButtons = document.querySelectorAll('#pokemonStats div');
+  //   const statQuery = {
+  //     name: false,
+  //     power: false,
+  //   };
+  const pokedexWrap = document.getElementById('pokedexWrap');
+  pokedexWrap.addEventListener('click', (e) => {
+    const target = e.target;
+    const data = target.getAttribute('data');
+    target.classList.add('pressed');
+    console.log(target);
+  });
 
-    let pokemonDisplay = document.getElementById('pokemonDisplay');
-    //select an item and assign a click function to it:
-    let goButton = document.getElementById('goButton');
-    goButton.addEventListener('click', function(evt) {
-        // $.ajax()
-        let name = document.getElementById('search').value;
-        name = name.toLowerCase();
-        // $.load();
-        console.log(name);
-    });
+  const backgroundImages = '0001 0002 0003 0004'.split(' ').map(imageName => `${imageName}.jpg`);
+  // how to animate the background-source?
+  //   const body = document.getElementsByTagName('body');
+  //   body.syle.background
+  //   document.getElementByID("divtest").style.backgroundImage = string;
+  // document.getElementByID("imgtest").src = string;
 });
-
-
-
-
-
