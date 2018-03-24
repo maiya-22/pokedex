375c421159e808f520e799f75983800baf34ff20
maiya-22 <maiya_o@yahoo.com>
2018-03-24 17:52:47 -0400

    making a request and retreiving a pokemon instance's picture

--- scripts/main-scripts.js
+++ scripts/main-scripts.js
@@ -1,22 +1,50 @@
 /* eslint func-names: 0,  no-unused-vars: 0, no-alert: 0 */
 $(() => {
+  // POKEMON CLASS
+  class Pokemon {
+    constructor(name, hp, attack, defense) {
+      this.name = name;
+      // this.hp = hp; // number'
+      // this.attack = attack;
+      // this.defense = defense;
+    }
+    makeRequest(pokemonName) {
+      const request = new XMLHttpRequest();
+      const endpoint = pokemonName || this.name || 'squirtle';
+      request.open('GET', `${this.baseUrl}/${endpoint}/`);
+      request.send();
+      request.onreadystatechange = function () {
+        if (request.readyState === 4) {
+          if (request.status === 200) {
+            // success function:
+            const data = JSON.parse(request.responseText);
+            const picUrl = data.sprites.front_default;
+
+            const floatingDisplay = document.getElementById('floatingDisplay');
+            const profilePic = document.createElement('img');
+            profilePic.src = picUrl;
+            profilePic.style.width = '100%';
+            floatingDisplay.appendChild(profilePic);
+            console.log(picUrl, profilePic, floatingDisplay);
+
+            // console.log(data);
+          } else {
+            // if there is an error:
+            alert('there was some error');
+            console.log('error message: ', request.statusText);
+          }
+        }
+      };
+    }
+  }
+  Pokemon.prototype.baseUrl = 'https://pokeapi.co/api/v2/pokemon';
+
+  // MAKE POKEMON INSTANCES:
+  const dragonair = new Pokemon('dragonair');
+  console.log(dragonair.baseUrl);
+  dragonair.makeRequest();
+
   // 'https://pokeapi.co/api/v2/pokemon'
-
-  // const pokemon = 'pikachu';
-
-  // $.ajax({
-  //   url: 'https://pokeapi.co/api/v2/pokemon/',
-  //   // dataType: 'jsonp',
-  //   success(data) {
-  //     const pokemonName = data.results;
-  //     console.log('pokemon name: ', data);
-
-  //     //
-  //   },
-  //   error() {
-  //     console.log('ERROR!');
-  //   },
-  // });
 
   // #POKEMON API CODE:
   const pokemonDisplay = document.getElementById('pokemonDisplay');
@@ -25,18 +53,10 @@ $(() => {
   const goButton = document.getElementById('goButton');
   goButton.addEventListener('click', (evt) => {
     // $.ajax()
-    const pokemonName = document.getElementById('search').value;
+    const pokemonName = document.getElementById('search').value.toLowerCase();
     console.log(pokemonName);
-    // pokemonName = pokemonName.toLowerCase();
-    // console.log(pokemonName);
   });
 
-  // make a list of stats to find and also change color of buttons when clicked:
-  //   const statButtons = document.querySelectorAll('#pokemonStats div');
-  //   const statQuery = {
-  //     name: false,
-  //     power: false,
-  //   };
   const pokedexWrap = document.getElementById('pokedexWrap');
   pokedexWrap.addEventListener('click', (e) => {
     const target = e.target;
@@ -47,7 +67,7 @@ $(() => {
 
   const backgroundImages = '0001 0002 0003 0004'.split(' ').map(imageName => `${imageName}.jpg`);
 
-  // #SET STYLE CODE:
+  // #SET POP-DOWN BOX CODE:
   const button = document.getElementById('set-style');
   // re-factor this as reusable function:
   const containerElements = 'frame one two three four five six'.split(' ');
@@ -67,7 +87,6 @@ $(() => {
     newSelect.setAttribute('value', className);
     classSelector.appendChild(newSelect);
   });
-
   button.addEventListener('click', (evt) => {
     evt.preventDefault();
     const selectedElement = document.getElementById('select-element');
@@ -79,41 +98,4 @@ $(() => {
   });
 
   // https://pokeapi.co/api/v2/pokemon/bulbasaur/
-
-  class Pokemon {
-    constructor(name, hp, attack, defense) {
-      this.name = name;
-      this.hp = hp; // number'
-      this.attack = attack;
-      this.defense = defense;
-    }
-    getPokemonByName(name) {
-      this.makeRequest(this.name);
-    }
-    makeRequest(query) {
-      const request = new XMLHttpRequest();
-      request.onreadystatechange = function () {
-        if (request.readyState === 4) {
-          if (request.status === 200) {
-            const data = JSON.parse(request.responseText);
-            // const myData = request.responseText;
-            console.log(data);
-            // console.log('status 200.');
-            // console.log('request.responseText:', request.responseText);
-            // alert(`${request.status} ${request.statusText}`);
-          } else {
-            alert('there was some error');
-          }
-        }
-      };
-      const endpoint = query || 'squirtle';
-      request.open('GET', `${this.baseUrl}/${endpoint}/`);
-      request.send();
-    }
-  }
-  Pokemon.prototype.baseUrl = 'https://pokeapi.co/api/v2/pokemon';
-  const testPokemon = new Pokemon();
-  console.log(testPokemon.baseUrl);
-
-  testPokemon.makeRequest();
 });
