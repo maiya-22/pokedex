65d9bdd483d0423e403d22358f523d5f588f99b3
maiya-22 <maiya_o@yahoo.com>
2018-03-24 19:54:45 -0400

    Promise working with JQuery and Ajax to fetch pokemon objects

--- scripts/main-scripts.js
+++ scripts/main-scripts.js
@@ -1,4 +1,4 @@
-/* eslint func-names: 0,  no-unused-vars: 0, no-alert: 0 */
+/* eslint func-names: 0,  no-unused-vars: 0, no-alert: 0, class-methods-use-this: 0 */
 $(() => {
   // POKEMON CLASS
   class Pokemon {
@@ -8,84 +8,91 @@ $(() => {
       // this.attack = attack;
       // this.defense = defense;
     }
-    fetchPokemonObject(pokemonName) {
-      const makeErrorGoAway = this.name;
-      $.ajax({
-        url: `https://pokeapi.co/api/v2/pokemon/${pokemonName}/`,
-        dataType: 'json',
-        success(data) {
-          console.log('data ', data);
-          return data;
-        },
-        error() {
-          console.log('ERROR');
-        },
+    pokePromise(pokemonName) {
+      return new Promise((resolve, reject) => {
+        $.ajax({
+          url: `https://pokeapi.co/api/v2/pokemon/${pokemonName}/`,
+          dataType: 'json',
+          success(data) {
+            console.log('ONE: data from fetchPokemonObject: ', data);
+            // alert(data);
+            resolve(data);
+          },
+          error(err) {
+            console.log('ERROR');
+            reject(err);
+          },
+        });
       });
     }
+    // fetchPokemonObject(pokemonName) {
+    //   $.ajax({
+    //     url: `https://pokeapi.co/api/v2/pokemon/${pokemonName}/`,
+    //     dataType: 'json',
+    //     success(data) {
+    //       console.log('data from fetchPokemonObject: ', data);
+    //       // alert(data);
+    //       return data;
+    //     },
+    //     error() {
+    //       console.log('ERROR');
+    //     },
+    //   });
+    // }
     makePokemonInstance(pokemonName) {
-      const pokemonInstance = new Pokemon(pokemonName);
-      console.log('pokemonInstance: ', pokemonInstance);
+      // this.fetchPokemonObject(pokemonName).then(data =>
+      //   console.log('hello from makepokeinst', data));
+      // fetch pokemon object
+      // then grab data
+      // then use data to make a new Pokemon instance
     }
 
-    // create pokemon instance:
+    // makeVanillaJSRequestPractice(pokemonName) {
+    //   const request = new XMLHttpRequest();
+    //   const endpoint = pokemonName || this.name || 'squirtle';
+    //   request.open('GET', `${this.baseUrl}/${endpoint}/`);
+    //   request.send();
+    //   request.onreadystatechange = function () {
+    //     if (request.readyState === 4) {
+    //       if (request.status === 200) {
+    //         // success function:
+    //         const data = JSON.parse(request.responseText);
+    //         const picUrl = data.sprites.front_default;
 
-    // display profile pic
+    //         const floatingDisplay = document.getElementById('floatingDisplay');
+    //         const profilePic = document.createElement('img');
+    //         profilePic.src = picUrl;
+    //         profilePic.style.width = '100%';
+    //         floatingDisplay.appendChild(profilePic);
+    //         // console.log(picUrl, profilePic, floatingDisplay);
 
-    // display stats
-
-    // $.ajax({
-    //   url: 'https://pokeapi.co/api/v2/pokemon/',
-    //   // dataType: 'jsonp',
-    //   success(data) {
-    //     const pokemonName = data.results;
-    //     console.log('pokemon name: ', data);
-
-    //     //
-    //   },
-    //   error() {
-    //     console.log('ERROR!');
-    //   },
-    // });
-
-    makeVanillaJSRequestPractice(pokemonName) {
-      const request = new XMLHttpRequest();
-      const endpoint = pokemonName || this.name || 'squirtle';
-      request.open('GET', `${this.baseUrl}/${endpoint}/`);
-      request.send();
-      request.onreadystatechange = function () {
-        if (request.readyState === 4) {
-          if (request.status === 200) {
-            // success function:
-            const data = JSON.parse(request.responseText);
-            const picUrl = data.sprites.front_default;
-
-            const floatingDisplay = document.getElementById('floatingDisplay');
-            const profilePic = document.createElement('img');
-            profilePic.src = picUrl;
-            profilePic.style.width = '100%';
-            floatingDisplay.appendChild(profilePic);
-            // console.log(picUrl, profilePic, floatingDisplay);
-
-            // console.log(data);
-          } else {
-            // if there is an error:
-            alert('there was some error');
-            // console.log('error message: ', request.statusText);
-          }
-        }
-      };
-    }
+    //         // console.log(data);
+    //       } else {
+    //         // if there is an error:
+    //         alert('there was some error');
+    //         // console.log('error message: ', request.statusText);
+    //       }
+    //     }
+    //   };
+    // }
   }
   Pokemon.prototype.baseUrl = 'https://pokeapi.co/api/v2/pokemon';
 
   // Make pokemon instance from prototype:
-  Pokemon.prototype.makePokemonInstance('dragonair');
+  //  Pokemon.prototype.makePokemonInstance('dragonair');
+  // Pokemon.prototype.fetchPokemonObject('dragonair'); // this one is working
+  Pokemon.prototype.pokePromise('dragonair').then((data) => {
+    console.log('TWO: ', data);
+    return data;
+    // return Pokemon.prototype.pokePromise('pikachu');
+  });
+  // .then(data => console.log('THREE: ', data));
 
   // MAKE POKEMON INSTANCES:
-  const dragonair = new Pokemon('dragonair');
+  // const dragonair = new Pokemon('dragonair');
   //  console.log(dragonair.baseUrl);
   // dragonair.makeVanillaJSRequestPractice();
-  dragonair.fetchPokemonObject('dragonair');
+  //  dragonair.fetchPokemonObject('dragonair');
   // 'https://pokeapi.co/api/v2/pokemon'
 
   // #POKEMON API CODE:
@@ -140,4 +147,10 @@ $(() => {
   });
 
   // https://pokeapi.co/api/v2/pokemon/bulbasaur/
+
+  // create pokemon instance:
+
+  // display profile pic
+
+  // display stats
 });
